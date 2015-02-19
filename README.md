ecf15689aba39f6205036837e95b26852f5e006f
Jonathan Reem <jonathan.reem@gmail.com>
2015-02-18 18:16:43 -0800

    (init)

---
+++ README.md
@@ -0,0 +1,22 @@
+# rcmut
+
+> An unchecked shared mutability primitive.
+
+## Usage
+
+Use the crates.io repository; add this to your `Cargo.toml` along
+with the rest of your dependencies:
+
+```toml
+[dependencies]
+rcmut = "*"
+```
+
+## Author
+
+[Jonathan Reem](https://medium.com/@jreem) is the primary author and maintainer of rcmut.
+
+## License
+
+MIT
+
---
+++ src/lib.rs
@@ -0,0 +1,45 @@
+#![cfg_attr(test, deny(warnings))]
+#![deny(missing_docs)]
+
+//! # RcMut
+//!
+//! An unchecked shared mutability primitive.
+//!
+//! RcMut provides unchecked shared mutability and ownership.
+//! It is extremely unsafe to use directly.
+//!
+
+use std::cell::UnsafeCell;
+use std::rc::Rc;
+use std::mem;
+
+/// A reference counted smart pointer with unrestricted mutability.
+pub struct RcMut<T> {
+    inner: Rc<UnsafeCell<T>>
+}
+
+impl<T> Clone for RcMut<T> {
+    fn clone(&self) -> RcMut<T> {
+        RcMut { inner: self.inner.clone() }
+    }
+}
+
+impl<T> RcMut<T> {
+    /// Create a new RcMut for a value.
+    pub fn new(val: T) -> RcMut<T> {
+        RcMut {
+            inner: Rc::new(UnsafeCell::new(val))
+        }
+    }
+
+    /// Get a reference to the value.
+    pub unsafe fn borrow(&self) -> &T {
+        mem::transmute(self.inner.get())
+    }
+
+    /// Get a mutable reference to the value.
+    pub unsafe fn borrow_mut(&mut self) -> &mut T {
+        mem::transmute(self.inner.get())
+    }
+}
+
