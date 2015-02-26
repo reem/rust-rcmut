ebbafd69f94d54dd22a210fc237b22c5124c6f69
Jonathan Reem <jonathan.reem@gmail.com>
2015-02-26 00:28:31 -0800

    (feat) Add ArcMut and as_[arc|rc]_mut? conversions.

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,7 @@
 //!
 
 use std::cell::UnsafeCell;
+use std::sync::Arc;
 use std::rc::Rc;
 use std::mem;
 
@@ -30,6 +31,56 @@ impl<T> RcMut<T> {
         RcMut {
             inner: Rc::new(UnsafeCell::new(val))
         }
+    }
+
+    /// Retrieve the inner Rc as a reference.
+    pub unsafe fn as_rc(&self) -> &Rc<T> {
+        mem::transmute(&self.inner)
+    }
+
+    /// Retrieve the inner Rc as a mutable reference.
+    pub unsafe fn as_rc_mut(&mut self) -> &mut Rc<T> {
+        mem::transmute(&mut self.inner)
+    }
+
+    /// Get a reference to the value.
+    pub unsafe fn borrow(&self) -> &T {
+        mem::transmute(self.inner.get())
+    }
+
+    /// Get a mutable reference to the value.
+    pub unsafe fn borrow_mut(&mut self) -> &mut T {
+        mem::transmute(self.inner.get())
+    }
+}
+
+/// A reference counted smart pointer with unrestricted mutability.
+pub struct ArcMut<T> {
+    inner: Arc<UnsafeCell<T>>
+}
+
+impl<T> Clone for ArcMut<T> {
+    fn clone(&self) -> ArcMut<T> {
+        ArcMut { inner: self.inner.clone() }
+    }
+}
+
+impl<T> ArcMut<T> {
+    /// Create a new ArcMut for a value.
+    pub fn new(val: T) -> ArcMut<T> {
+        ArcMut {
+            inner: Arc::new(UnsafeCell::new(val))
+        }
+    }
+
+    /// Retrieve the inner Rc as a reference.
+    pub unsafe fn as_arc(&self) -> &Arc<T> {
+        mem::transmute(&self.inner)
+    }
+
+    /// Retrieve the inner Rc as a mutable reference.
+    pub unsafe fn as_arc_mut(&mut self) -> &mut Arc<T> {
+        mem::transmute(&mut self.inner)
     }
 
     /// Get a reference to the value.
